07503d8edf5551e3b8e43d94584f1bfe22b0f309
guillaumeleparmentierpro <guillaume.leparmentier.pro@gmail.com>
2026-05-08 12:06:20 +0200

    Create menu-data.js

---
+++ menu-data.js
@@ -0,0 +1,33 @@
+// --- MENU DU JOUR : FICHIER DE MODIFICATION ---
+// Consigne : Modifiez uniquement les textes situés entre les guillemets anglais " ". 
+// Ne supprimez pas les virgules à la fin des lignes.
+
+const menuDuJour = {
+    date: "Aujourd'hui au Chalet Gourmand",
+    
+    entrees: [
+        { 
+            nom: "Soupe de Poissons Maison", 
+            desc: "Accompagnée de sa rouille, emmental râpé et de croûtons", 
+            prix: "8.50 €" 
+        },
+        { 
+            nom: "Rillettes aux deux Saumons", 
+            desc: "Recette artisanale d'Anne-Laure", 
+            prix: "7.00 €" 
+        }
+    ],
+    
+    plats: [
+        { 
+            nom: "Choucroute de la Mer", 
+            desc: "Chou au vin blanc, saumon, cabillaud, haddock fumé et crevettes", 
+            prix: "18.50 €" 
+        },
+        { 
+            nom: "Bouchée à la Reine aux Fruits de Mer", 
+            desc: "Saint-Jacques, crevettes et moules dans un feuilleté pur beurre", 
+            prix: "14.00 €" 
+        }
+    ]
+};
