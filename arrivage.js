8226a60d16e722ac2b1106465ff51eb64d461d55
guillaumeleparmentierpro <guillaume.leparmentier.pro@gmail.com>
2026-05-19 12:28:15 +0200

    Update arrivage.js

--- arrivage.js
+++ arrivage.js
@@ -1,3 +1,3 @@
 // Il suffit de changer le texte entre les guillemets !
 // Pour cacher le bandeau, laissez simplement les guillemets vides : ""
-const texteArrivage = "🦞 Arrivage du jour : Homard bleu de casier et Huîtres de Denneville !";
+const texteArrivage = "";
