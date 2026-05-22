0e2ce7651e6b93df39d12b3a4e20369add0fe273
guillaumeleparmentierpro <guillaume.leparmentier.pro@gmail.com>
2026-05-22 13:57:30 +0200

    Update arrivage.js

--- arrivage.js
+++ arrivage.js
@@ -1,3 +1,3 @@
 // Il suffit de changer le texte entre les guillemets !
 // Pour cacher le bandeau, laissez simplement les guillemets vides : ""
-const texteArrivage = "";
+const texteArrivage = "20 mai 2026 - Saumon de Cherbourg à seulement 19,90€/kg";
