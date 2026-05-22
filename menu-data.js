8e295ff7f1845f913a246c1bbba6e3e5e428cf4b
guillaumeleparmentierpro <guillaume.leparmentier.pro@gmail.com>
2026-05-22 14:24:23 +0200

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -26,18 +26,18 @@ const menuDuJour = {
     plats: [
         { 
             nom: "Coquillages de l'écailler", 
-            desc: "1 demie araignée (ou 2 pinces de tourteau), 6 huîtres, 6 crevettes roses, 12 bulots, Bigornaux", 
+            desc: "1 demie araignée (ou 2 pinces de tourteau), 6 huîtres, 6 crevettes roses, 12 bulots, Bigornaux, (Offert : Citron, Mayonnaise, Rince doigts, Pic à bulots)", 
             prix: "26.90 €/personne (Offert : Citron, Mayonnaise, Rince doigts, Pic à bulots)" 
         },
         { 
             nom: "Les Fiertés de la Mer", 
-            desc: "1 demi araignée (ou 2 pinces de tourteau), 6 huîtres, 2 langoustines, 6 crevettes roses, 12 bulots, Bigornaux, crevettes grises", 
-            prix: "36.90 €/personne (Offert : Citron, Mayonnaise, Rince doigts, Pic à bulots)" 
+            desc: "1 demi araignée (ou 2 pinces de tourteau), 6 huîtres, 2 langoustines, 6 crevettes roses, 12 bulots, Bigornaux, crevettes grises, (Offert : Citron, Mayonnaise, Rince doigts, Pic à bulots", 
+            prix: "36.90 €/pers." 
         },
         { 
             nom: "Royal de l'Océan", 
-            desc: "1/2 homard, 2 langoustines, 6 huîtres, 6 crevettes, 12 bulots, Bigornaux, crevettes grises", 
-            prix: "56.90 €/personne (Offert : Citron, Mayonnaise, Rince doigts, Pic à bulots)" 
+            desc: "1/2 homard, 2 langoustines, 6 huîtres, 6 crevettes, 12 bulots, Bigornaux, crevettes grises, (Offert : Citron, Mayonnaise, Rince doigts, Pic à bulots", 
+            prix: "56.90 €/pers." 
            
         }
     ]
